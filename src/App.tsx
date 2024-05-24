bb89844e929af0b1a7ab2e5e18e17653e22a3d3f
Hestoria Lee <sam.252787@hotmail.com>
2024-05-23 21:32:43 -0700

    added motion for animations for compos

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
 import SideBySideLayout from "@/components/layout/sidebyside";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import React from "react";
-import { Button } from "./components/ui/button";
+
+import Hero from "@/components/session/hero";
+
 interface AppProps {}
 
 export const App: React.FC<AppProps> = ({}) => {
   return (
     <main className="xl:flex xl:overflow-hidden h-screen">
       <SideBySideLayout
-        leftSession={
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>Create project</CardTitle>
-              <CardDescription>
-                Deploy your new project in one-click.
-              </CardDescription>
-            </CardHeader>
-
-            <CardFooter className="flex justify-between">
-              <Button variant="outline">Cancel</Button>
-              <Button>Deploy</Button>
-            </CardFooter>
-          </Card>
-        }
+        leftSession={<Hero />}
         rightSession={
           <div className="scrollableContent">
             {[...Array(20).keys()].map((index) => (
