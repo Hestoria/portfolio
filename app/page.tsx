37be2d0c331ad2a54a8454cb80c852251fe94e77
Hestoria Lee <sam.252787@hotmail.com>
2024-07-08 16:35:52 -0700

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import {
+  ContactPage,
+  HeroPage,
+  ProjectsPage,
+  ResumePage,
+  ServicesPage,
+} from "@/components/pages";
+
 export default function Home() {
-  return <main>homepage</main>;
+  return (
+    <main>
+      <HeroPage />
+      <ServicesPage />
+      <ResumePage />
+      <ProjectsPage />
+      <ContactPage />
+    </main>
+  );
 }
---
+++ components/pages/Hero.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface heroProps {}
+
+export const hero: React.FC<heroProps> = ({}) => {
+  return <div>hero</div>;
+};
+
+export default hero;
