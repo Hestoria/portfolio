6cc9cacc67f94510903b03f31398ed54ea654a57
Hestoria Lee <sam.252787@hotmail.com>
2024-09-17 22:33:46 -0700

    need a dark bg

---
+++ src/components/ui/box-reveal.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+interface BoxRevealProps {
+  children: JSX.Element;
+  width?: "fit-content" | "100%";
+  boxColor?: string;
+  duration?: number;
+  className?: string;
+  delay?: number;
+}
+
+export const BoxReveal = ({
+  children,
+  width = "fit-content",
+  boxColor,
+  duration,
+  delay,
+  className,
+}: BoxRevealProps) => {
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      slideControls.start("visible");
+      mainControls.start("visible");
+    } else {
+      slideControls.start("hidden");
+      mainControls.start("hidden");
+    }
+  }, [isInView, mainControls, slideControls]);
+
+  return (
+    <div
+      ref={ref}
+      className={className}
+      style={{ position: "relative", width, overflow: "hidden" }}
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{
+          duration: duration ? duration : 0.5,
+          delay: delay ? delay + 0.25 : 0.25,
+        }}
+      >
+        {children}
+      </motion.div>
+
+      <motion.div
+        className="via-purple-500 from-blue-500 bg-gradient-to-r"
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{
+          duration: duration ? duration : 0.5,
+          ease: "easeIn",
+          delay: delay ?? 0.25,
+        }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          zIndex: 20,
+        }}
+      />
+    </div>
+  );
+};
+
+export default BoxReveal;
