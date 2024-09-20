672d61f0af95095e7babb52aa5b7ac56c54a5c3e
Sam Lee <sam.252787@hotmail.com>
2024-09-20 00:50:49 -0700

    init ani

--- src/components/pages/initPage.tsx
+++ src/components/pages/initPage.tsx
@@ -1,23 +1,24 @@
-import { motion } from "framer-motion"
-import LoadingBanner, { Props as LoadingProps } from "@/components/ui/loading-banner"
+import LoadingBanner, {
+  Props as LoadingProps,
+} from "@/components/ui/loading-banner";
+import { motion } from "framer-motion";
 
-interface Props extends LoadingProps { };
+interface Props extends LoadingProps {}
 
 const initPage = (props: Props) => {
   return (
     <motion.div
       key="initPage"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 1.5 } }}
-      exit={{ opacity: 0.25, transition: { duration: 1.5 } }}
+      exit={{
+        // TODO edit exit animation
+        transition: { duration: 2.5, delay: 2.5 },
+      }}
       className="bg-cover bg-center w-full h-screen"
       style={{ backgroundImage: `url('bg2.jpg')` }}
     >
-      <LoadingBanner
-        {...props}
-      />
+      <LoadingBanner {...props} />
     </motion.div>
-  )
-}
+  );
+};
 
 export default initPage;
