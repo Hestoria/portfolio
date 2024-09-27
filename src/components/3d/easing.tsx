9dbd0722f69897da1ee6f409682ca896c9f25e82
Sam Lee <54168461+Hestoria@users.noreply.github.com>
2024-09-27 18:11:38 +0000

    small fix

--- src/components/3d/easing.tsx
+++ src/components/3d/easing.tsx
@@ -10,7 +10,7 @@ const Easing: React.FC<Props> = ({ children }) => {
   const group = useRef<any>(null);
 
   useFrame((state, delta) => {
-    // easing.damp3(state.camera.position, [0, 0, 8], 0.25, delta);
+    // easing.damp3(state.camera.position, [0, 0, 5], 0.25, delta);
 
     easing.dampE(
       group.current.rotation,
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -5,39 +5,28 @@ import {
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { motion } from "framer-motion";
+import { Suspense } from "react";
 import CatModel from "../3d/cat";
 import Easing from "../3d/easing";
 
+const _animateProps ={
+  hidden:{ opacity: 0, y: -30 },
+  show:{
+    opacity: 1,
+    y: 0,
+  },
+}
+// transition:{ duration: 1.5, ease: "easeInOut" }
+
 const Hero = () => {
   return (
-    <section id="hero" className="h-screen">
-      {/* Background Image Div */}
-      <motion.div className="absolute inset-0 bg-cover bg-center h-screen px-4 z-[30]">
-        <Canvas>
-          <OrbitControls
-            enablePan={false}
-            enableZoom={false}
-            minPolarAngle={Math.PI / 2}
-            maxPolarAngle={Math.PI / 2}
-          />
-          <PerspectiveCamera makeDefault position={[0, 0, 8]} />
-          <Easing>
-            <CatModel
-              position={[0, -2, 0]}
-              // make scale x bigger so the cat is fatter LOL
-              scale={[7, 6, 6]}
-              rotation={[0, 0.25, 0]}
-            />
-          </Easing>
-
-          <Environment preset="city" />
-        </Canvas>
-      </motion.div>
-
+    <section id="hero" className="aspect-[16/9]">
       {/* Content Div  */}
-      <div className="absolute w-full flex items-center flex-col">
-        <div className="md:mt-48 sm:mt-52 mt-52 px-4 w-3/4">
-          <p className="text-center text-pretty uppercase xl:text-3xl md:text-2xl sm:text-xl text-lg font-black text-white font-mono">
+      <motion.div initial={"hidden"} animate={"show"} variants={{show:{
+        transition:{staggerChildren:0.3,duration: 1.5, ease: "easeInOut"},
+      }}} className="w-full flex items-center flex-col h-[40vh]">
+        <div className="mt-36 px-4 w-3/4">
+          <motion.p  variants={_animateProps}  className="text-center text-pretty uppercase xl:text-3xl md:text-2xl sm:text-xl text-lg font-black text-white font-mono">
             Hello, I am Sam{" "}
             <motion.span
               className="inline-block origin-[70%_70%]"
@@ -53,14 +42,37 @@ const Hero = () => {
             >
               👋
             </motion.span>
-          </p>
+          </motion.p>
         </div>
         <div className="sm:mt-26 mt-12 px-4 w-1/2">
-          <p className="text-center text-pretty xl:text-2xl md:text-xl sm:text-lg text-base font-mono">
-            I am a Software Engineer with years of experience creating in
-            developing robust, scalable applications.
-          </p>
+          <motion.p  variants={_animateProps} className="text-center text-pretty xl:text-2xl md:text-xl sm:text-lg text-base font-mono">
+          I'm a Software Engineer driven by curiosity and a passion for crafting elegant, efficient.
+          </motion.p>
         </div>
+      </motion.div>
+
+       {/* Background Image Div */}
+       <div className="bg-cover bg-center h-[60vh] px-4">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Canvas>
+            <OrbitControls
+              enablePan={false}
+              enableZoom={false}
+              minPolarAngle={Math.PI / 2}
+              maxPolarAngle={Math.PI / 2}
+            />
+            <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+            <Easing>
+              <CatModel
+                position={[0, -1, 0]}
+                // make scale x bigger so the cat is fatter LOL
+                scale={[7, 6, 6]}
+                rotation={[0, 0.25, 0]}
+              />
+            </Easing>
+            <Environment preset="city" />
+          </Canvas>
+        </Suspense>
       </div>
     </section>
   );
